065254287263acd9dee54fcc0bfe89e5fd1f632b
johnny1204 <major@za2.so-net.ne.jp>
2021-09-05 20:34:34 +0900

    カートモーダルの既存品のAPIからの取得

--- front/src/containers/templates/CartModal.tsx
+++ front/src/containers/templates/CartModal.tsx
@@ -1,17 +1,40 @@
-import { FC, useState } from 'react';
+import axios from 'axios';
+import { FC, useEffect, useState } from 'react';
 import { Button, Icon, Modal } from 'semantic-ui-react';
 import CartModalForm from '../molecules/CartModalForm';
-import { foodStuffs } from '../../data/data';
+import { FoodStuffApiResponse } from '../../data/data';
 
 const CartModal: FC = () => {
 	const [open, setOpen] = useState(false);
 	const [disabled, setDisableSubmit] = useState(true);
 
-	const dropDownOptions = foodStuffs.map((type) => ({
-		key: type.name,
-		text: type.name,
-		value: type.name,
-	}));
+	type foodsOption = {
+		key: string;
+		text: string;
+		value: string;
+	};
+	const [foods, setFoods] = useState<foodsOption[]>();
+	useEffect(() => {
+		// eslint-disable-next-line prefer-arrow/prefer-arrow-functions
+		async function getFoods() {
+			const resp = await axios.get<FoodStuffApiResponse>(
+				'http://localhost:8080/api/food_stuff'
+			);
+
+			return resp;
+		}
+
+		void getFoods()
+			.then((resp) => resp.data)
+			.then((data) =>
+				data.data.map((food) => ({
+					key: food.name,
+					text: food.name,
+					value: food.name,
+				}))
+			)
+			.then((foodStuffs) => setFoods(foodStuffs));
+	}, []);
 
 	return (
 		<Modal
@@ -27,12 +50,16 @@ const CartModal: FC = () => {
 			}
 		>
 			<Modal.Header>カート追加</Modal.Header>
-			<Modal.Content>
-				<CartModalForm
-					options={dropDownOptions}
-					disabledSubmit={(result) => setDisableSubmit(result)}
-				/>
-			</Modal.Content>
+			{foods ? (
+				<Modal.Content>
+					<CartModalForm
+						options={foods}
+						disabledSubmit={(result) => setDisableSubmit(result)}
+					/>
+				</Modal.Content>
+			) : (
+				<Modal.Content />
+			)}
 			<Modal.Actions>
 				<Button
 					color="green"
--- front/src/data/data.ts
+++ front/src/data/data.ts
@@ -5,41 +5,6 @@ export type FoodStuff = {
 	stock: number;
 };
 
-type FoodStuffs = FoodStuff[];
-
-export const foodStuffs: FoodStuffs = [
-	{
-		id: 1,
-		name: '鶏もも肉',
-		type: 'meat',
-		stock: 1,
-	},
-	{
-		id: 2,
-		name: '玉ねぎ',
-		type: 'vegetable',
-		stock: 3,
-	},
-	{
-		id: 3,
-		name: 'にんじん',
-		type: 'vegetable',
-		stock: 2,
-	},
-	{
-		id: 4,
-		name: 'じゃがいも',
-		type: 'vegetable',
-		stock: 1,
-	},
-	{
-		id: 5,
-		name: '鮭',
-		type: 'fish',
-		stock: 1,
-	},
-];
-
 export type FoodType = {
 	name: string;
 	type: string;
